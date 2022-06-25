a4978400255b98c88e6b500c56601f01f445fe8d
Nir Gazit <nir.gzt@gmail.com>
2022-06-25 13:39:14 -0400

    Attempt to remove static optimizations

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,16 @@
-import '../../styles/globals.css'
+import App from "next/app";
+import "../../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    return { pageProps };
+  }
 }
 
-export default MyApp
+export default MyApp;
